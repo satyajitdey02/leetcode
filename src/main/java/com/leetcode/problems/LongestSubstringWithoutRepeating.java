ad47eadee1602b50a3e29599a8f863e7f1f672d9
Satyajit Dey <satyajit02@gmail.com>
2018-09-07 19:12:58 +0600

    3: Accepted - 395ms

--- src/main/java/com/leetcode/problems/LongestSubstringWithoutRepeating.java
+++ src/main/java/com/leetcode/problems/LongestSubstringWithoutRepeating.java
@@ -2,13 +2,13 @@ package com.leetcode.problems;
 
 public class LongestSubstringWithoutRepeating {
 
-    public int lengthOfLongestSubstring(String str) {
-        String s = str.substring(0, str.length());
+    public int lengthOfLongestSubstring(String s) {
+        String cloneS = s.substring(0, s.length());
         int result = 0, index = 0;
-        char[] chars = s.toCharArray();
+        char[] chars = cloneS.toCharArray();
         StringBuilder sb = new StringBuilder();
 
-        while (index < s.length()) {
+        while (index < cloneS.length()) {
             int repeatIndex = sb.toString().indexOf(chars[index]);
             if (repeatIndex < 0) {
                 sb.append(chars[index]);
@@ -23,9 +23,9 @@ public class LongestSubstringWithoutRepeating {
             String sub = sb.toString() + chars[index];
 
 
-            index = s.indexOf(sub) + repeatIndex + 1;//findNextNonRepeatChar(s, s.indexOf(sub) + repeatIndex + 1);
-            s = s.substring(index, s.length()-1);
-            chars = s.toCharArray();
+            index = cloneS.indexOf(sub) + repeatIndex + 1;//findNextNonRepeatChar(s, s.indexOf(sub) + repeatIndex + 1);
+            cloneS = cloneS.substring(index, cloneS.length());
+            chars = cloneS.toCharArray();
             index=0;
             sb.setLength(0);
 
@@ -63,7 +63,7 @@ public class LongestSubstringWithoutRepeating {
         LongestSubstringWithoutRepeating instance = new LongestSubstringWithoutRepeating();
         System.out.println("gpaazszqlcxvfoaaxzagwsemzszjukeczgfbzetkisiq: " + instance.lengthOfLongestSubstring("gpaazszqlcxvfoaaxzagwsemzszjukeczgfbzetkisiq"));
 
-        /* System.out.println("aab: "+ instance.lengthOfLongestSubstring("aab"));
+        System.out.println("aab: "+ instance.lengthOfLongestSubstring("aab"));
          System.out.println("satyajit: " + instance.lengthOfLongestSubstring("satyajit"));
          System.out.println("keya: " +instance.lengthOfLongestSubstring("keya"));
         System.out.println("kuhu: " + instance.lengthOfLongestSubstring("kuhu"));
@@ -71,6 +71,6 @@ public class LongestSubstringWithoutRepeating {
         System.out.println("bbbbbb: " + instance.lengthOfLongestSubstring("bbbbbb"));
         System.out.println("mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt: " + instance.lengthOfLongestSubstring("mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt"));
 
-*/
+
     }
 }
