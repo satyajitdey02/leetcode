2ad4cc5a83ef95f2a7285ed23d4d21d31d30924a
Satyajit Dey <satyajit02@gmail.com>
2018-09-07 23:21:10 +0600

    3: Code Both self and leetcode solution

--- src/main/java/com/leetcode/problems/LongestSubstringWithoutRepeating.java
+++ src/main/java/com/leetcode/problems/LongestSubstringWithoutRepeating.java
@@ -1,5 +1,8 @@
 package com.leetcode.problems;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstringWithoutRepeating {
 
     public int lengthOfLongestSubstring(String s) {
@@ -20,29 +23,57 @@ public class LongestSubstringWithoutRepeating {
 
             index = cloneS.indexOf(sub) + repeatIndex + 1;
             cloneS = cloneS.substring(index, cloneS.length());
-            if(cloneS.length() <= result) {
+            if (cloneS.length() <= result) {
                 return result;
             }
 
-            index=0;
+            index = 0;
             sb.setLength(0);
         }
 
         return sb.toString().length();
     }
 
+    public int lengthOfLongestSubstringLeetcodeSolution(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         LongestSubstringWithoutRepeating instance = new LongestSubstringWithoutRepeating();
 
-        System.out.println("gpaazszqlcxvfoaaxzagwsemzszjukeczgfbzetkisiq: " + instance.lengthOfLongestSubstring("gpaazszqlcxvfoaaxzagwsemzszjukeczgfbzetkisiq"));
-        System.out.println("aab: " + instance.lengthOfLongestSubstring("aab"));
-        System.out.println("satyajit: " + instance.lengthOfLongestSubstring("satyajit"));
-        System.out.println("keya: " + instance.lengthOfLongestSubstring("keya"));
-        System.out.println("kuhu: " + instance.lengthOfLongestSubstring("kuhu"));
-        System.out.println("abcabcbb: " + instance.lengthOfLongestSubstring("abcabcbb"));
-        System.out.println("bbbbbb: " + instance.lengthOfLongestSubstring("bbbbbb"));
-        System.out.println("mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt: " + instance.lengthOfLongestSubstring("mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt"));
+        //System.out.println("gpaazszqlcxvfoaaxzagwsemzszjukeczgfbzetkisiq: " + instance.lengthOfLongestSubstringLeetcodeSolution("gpaazszqlcxvfoaaxzagwsemzszjukeczgfbzetkisiq"));
+        // System.out.println("aab: " + instance.lengthOfLongestSubstringLeetcodeSolution("aab"));
 
+        String input = "mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt";
+        long startTime = System.nanoTime();
+        int answer = instance.lengthOfLongestSubstringLeetcodeSolution(input);
+        long endTime = System.nanoTime();
+        System.out.println(String.format("%s - %s - %s", input, answer, (endTime - startTime)));
+
+        input = "mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt";
+        startTime = System.nanoTime();
+        answer = instance.lengthOfLongestSubstring(input);
+        endTime = System.nanoTime();
+        System.out.println(String.format("%s - %s - %s", input, answer, (endTime - startTime)));
+        /*
+        System.out.println("keya: " + instance.lengthOfLongestSubstringLeetcodeSolution("keya"));
+        System.out.println("kuhu: " + instance.lengthOfLongestSubstringLeetcodeSolution("kuhu"));
+        System.out.println("abcabcbb: " + instance.lengthOfLongestSubstringLeetcodeSolution("abcabcbb"));
+        System.out.println("bbbbbb: " + instance.lengthOfLongestSubstringLeetcodeSolution("bbbbbb"));
+        System.out.println("mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt: " + instance.lengthOfLongestSubstringLeetcodeSolution("mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt"));
+*/
 
     }
 }
