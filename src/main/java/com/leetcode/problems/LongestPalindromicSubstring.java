7f771ce339692b8d7f9b882037d846bba5994638
Satyajit Dey <satyajit02@gmail.com>
2018-09-10 00:28:50 +0600

    5: no improvements

--- src/main/java/com/leetcode/problems/LongestPalindromicSubstring.java
+++ src/main/java/com/leetcode/problems/LongestPalindromicSubstring.java
@@ -7,32 +7,56 @@ public class LongestPalindromicSubstring {
     int index = 0;
     String palindrom = "";
     while (index < s.length()) {
-      char current = s.charAt(index);
-      /*if(current == 'm') {
-        System.out.println("Current: " + current);
-      }*/
+      StringBuilder sb = new StringBuilder();
+      //palindrom = s.substring(index,index+1);
+      int k = index;
 
-      if (index > 0 && index < s.length() - 1) {
-        if (s.charAt(index - 1) == s.charAt(index + 1)/* && palindrom.length() <= 3*/) {
-          palindrom = s.substring(index - 1, (index + 1) + 1);
-          int i = index - 2;
-          int j = index + 2;
-          while (i >= 0 && j < s.length()) {
-            if (s.charAt(i) != s.charAt(j)) {
-              break;
-            }
+      while (k < s.length()) {
+        if (k + 1 >= s.length()) {
+          break;
+        }
 
-            //System.out.println("length: " + (j - i + 1));
-            palindrom = s.substring(i, j + 1);
-            i--;
-            j++;
+        if (s.charAt(k) != s.charAt(k + 1)) {
+          break;
+        }
 
+        sb.append(s.charAt(k));
+        k++;
+      }
+
+      if (sb.toString().length() > 0) {
+        sb.append(s.charAt(k));
+        palindrom = sb.toString();
+        index = k;//index + palindrom.length();
+      }
+
+
+      int leftIndex = index - sb.toString().length();
+      int rightIndex = index + sb.toString().length()-1;
+      if(rightIndex < 0) {
+        rightIndex = 0;
+      }
+      if (index >= sb.toString().length() && index < s.length() && s.charAt(leftIndex) == s.charAt(rightIndex)) {
+
+        //int from = index - sb.toString().length();
+       // int to = (index + sb.toString().length()) + 1;
+        palindrom = s.substring(leftIndex, rightIndex+1);
+
+        int i = index - 2;
+        int j = index + 2+sb.toString().length();
+        while (i >= 0 && j < s.length()) {
+          if (s.charAt(i) != s.charAt(j)) {
+            break;
           }
 
-          result = result.length() < palindrom.length() ? palindrom : result;
+          palindrom = s.substring(i, j + 1);
+          i--;
+          j++;
+
         }
       }
 
+      result = result.length() < palindrom.length() ? palindrom : result;
       index++;
     }
 
@@ -42,5 +66,6 @@ public class LongestPalindromicSubstring {
   public static void main(String[] args) {
     LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
     System.out.println(longestPalindromicSubstring.longestPalindrome("allpadapllagogabamabagog"));
+    System.out.println(longestPalindromicSubstring.longestPalindrome("alla"));
   }
 }
