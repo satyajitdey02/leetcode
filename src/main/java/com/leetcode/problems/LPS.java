51001bba63c97edb867fa33bf66ee36b4fab07b9
Satyajit Dey <satyajit02@gmail.com>
2018-09-10 23:02:33 +0600

    5: Accepted - 65ms

--- src/main/java/com/leetcode/problems/LPS.java
+++ src/main/java/com/leetcode/problems/LPS.java
@@ -6,15 +6,42 @@ package com.leetcode.problems;
 public class LPS {
 
   public static String longestPalindrome(String s) {
-    String result = "", palindrom;
+    String result = "", palindrom = "";
     int index = 0;
 
     while (index < s.length()) {
-      if (index % 2 == 0) {
-        palindrom = extractPalindrom(index - 1, index + 1, s);
-      } else {
+      int end = findStopIndex(index, s);
+      if (index == end && index == 0) {
+        palindrom = s.charAt(index) + "";
+        /*index++;
+        continue;*/
+      } else if (index == end && index > 0) {
+        int i = index - 1;
+        int j = index + 1;
+        while (i >= 0 && j < s.length()) {
+          if (s.charAt(i) != s.charAt(j)) {
+            break;
+          }
 
-        palindrom = extractPalindrom(index, index + 1, s);
+          palindrom = s.substring(i, j + 1);
+          i--;
+          j++;
+        }
+
+      } else {
+        palindrom = s.substring(index, end + 1);
+        int i = index == 0 ? 0 : index - 1;
+        //end = end == 0 ? 0 : end - 1;
+        int j = end == s.length() - 1 ? s.length() - 1 : end + 1;
+
+        while (i >= 0 && j < s.length()) {
+          if (s.charAt(i) != s.charAt(j)) {
+            break;
+          }
+          palindrom = s.substring(i, j + 1);
+          i--;
+          j++;
+        }
       }
 
       if (palindrom.length() > result.length()) {
@@ -27,23 +54,24 @@ public class LPS {
     return result;
   }
 
-  private static String extractPalindrom(int i, int j, String s) {
-    String palindrom = "";
-    while (i >= 0 && j < s.length()) {
-      if (s.charAt(i) != s.charAt(j)) {
-        return palindrom;
+  private static int findStopIndex(int start, String s) {
+    int index = start;
+    int end = start;
+    while (index < s.length() - 1) {
+      if (s.charAt(index) != s.charAt(index + 1)) {
+        break;
       }
 
-      palindrom = s.substring(i, j + 1);
-      i--;
-      j++;
+      end++;
+      index++;
     }
 
-    return palindrom;
+    return end;
   }
 
   public static void main(String[] args) {
-    //System.out.println(longestPalindrome("madam"));
+    System.out.println(longestPalindrome("madamaamadam"));
     System.out.println(longestPalindrome("aabbaa"));
+    System.out.println(longestPalindrome("cbbd"));
   }
 }
