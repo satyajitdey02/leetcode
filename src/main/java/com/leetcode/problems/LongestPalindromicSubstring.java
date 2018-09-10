46e4486627e7be30336060359c31fcfcbb5b033e
Satyajit Dey <satyajit02@gmail.com>
2018-09-10 23:33:34 +0600

    5: Accepted - 55ms

--- src/main/java/com/leetcode/problems/LongestPalindromicSubstring.java
+++ src/main/java/com/leetcode/problems/LongestPalindromicSubstring.java
@@ -3,47 +3,16 @@ package com.leetcode.problems;
 public class LongestPalindromicSubstring {
 
   public String longestPalindrome(String s) {
-    String result = "";
+    String result = "", palindrom = "";
     int index = 0;
-    String palindrom = "";
+
     while (index < s.length()) {
-      StringBuilder sb = new StringBuilder();
-      //palindrom = s.substring(index,index+1);
-      int k = index;
-
-      while (k < s.length()) {
-        if (k + 1 >= s.length()) {
-          break;
-        }
-
-        if (s.charAt(k) != s.charAt(k + 1)) {
-          break;
-        }
-
-        sb.append(s.charAt(k));
-        k++;
-      }
-
-      if (sb.toString().length() > 0) {
-        sb.append(s.charAt(k));
-        palindrom = sb.toString();
-        index = k;//index + palindrom.length();
-      }
-
-
-      int leftIndex = index - sb.toString().length();
-      int rightIndex = index + sb.toString().length()-1;
-      if(rightIndex < 0) {
-        rightIndex = 0;
-      }
-      if (index >= sb.toString().length() && index < s.length() && s.charAt(leftIndex) == s.charAt(rightIndex)) {
-
-        //int from = index - sb.toString().length();
-       // int to = (index + sb.toString().length()) + 1;
-        palindrom = s.substring(leftIndex, rightIndex+1);
-
-        int i = index - 2;
-        int j = index + 2+sb.toString().length();
+      int end = findStopIndex(index, s);
+      if (index == end && index == 0) {
+        palindrom = s.charAt(index) + "";
+      } else if (index == end && index > 0) {
+        int i = index - 1;
+        int j = index + 1;
         while (i >= 0 && j < s.length()) {
           if (s.charAt(i) != s.charAt(j)) {
             break;
@@ -52,20 +21,55 @@ public class LongestPalindromicSubstring {
           palindrom = s.substring(i, j + 1);
           i--;
           j++;
+        }
 
+      } else {
+        palindrom = s.substring(index, end + 1);
+        int i = index == 0 ? 0 : index - 1;
+        //end = end == 0 ? 0 : end - 1;
+        int j = end == s.length() - 1 ? s.length() - 1 : end + 1;
+
+        while (i >= 0 && j < s.length()) {
+          if (s.charAt(i) != s.charAt(j)) {
+            break;
+          }
+          palindrom = s.substring(i, j + 1);
+          i--;
+          j++;
         }
       }
 
-      result = result.length() < palindrom.length() ? palindrom : result;
+      if (palindrom.length() > result.length()) {
+        result = palindrom.substring(0, palindrom.length());
+      }
+
       index++;
     }
 
     return result;
   }
 
+  private  int findStopIndex(int start, String s) {
+    int index = start;
+    int end = start;
+    while (index < s.length() - 1) {
+      if (s.charAt(index) != s.charAt(index + 1)) {
+        break;
+      }
+
+      end++;
+      index++;
+    }
+
+    return end;
+  }
+
   public static void main(String[] args) {
-    LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
-    System.out.println(longestPalindromicSubstring.longestPalindrome("allpadapllagogabamabagog"));
-    System.out.println(longestPalindromicSubstring.longestPalindrome("alla"));
+    LongestPalindromicSubstring palindromicSubstring = new LongestPalindromicSubstring();
+    System.out.println(palindromicSubstring.longestPalindrome("allpadapllagogabamabagog"));
+    System.out.println(palindromicSubstring.longestPalindrome("alla"));
+    System.out.println(palindromicSubstring.longestPalindrome("madamaamadam"));
+    System.out.println(palindromicSubstring.longestPalindrome("aabbaa"));
+    System.out.println(palindromicSubstring.longestPalindrome("cbbd"));
   }
 }
