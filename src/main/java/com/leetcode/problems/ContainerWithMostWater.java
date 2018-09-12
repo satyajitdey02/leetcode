5b39d24984a530092bff1f75288016033e2dc963
Satyajit Dey <satyajit02@gmail.com>
2018-09-12 23:21:01 +0600

    11: Added O(n) solution

--- src/main/java/com/leetcode/problems/ContainerWithMostWater.java
+++ src/main/java/com/leetcode/problems/ContainerWithMostWater.java
@@ -12,10 +12,32 @@ public class ContainerWithMostWater {
     return result;
   }
 
+  public int maxArea_On(int[] height) {
+    int result = 0, left = 0, right = height.length - 1;
+    while (left != right) {
+      result = Math.max(result, Math.min(height[left], height[right]) * (right - left));
+      if (height[left] > height[right]) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+
+    return result;
+  }
+
   public static void main(String[] args) {
     int[] input = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+    long start = System.nanoTime();
     int result = new ContainerWithMostWater().maxArea(input);
-    System.out.println("result = " + result);
+    long end = System.nanoTime();
+    System.out.println("maxArea = " + result + ", ET = " + (end-start));
+
+    start = System.nanoTime();
+    result = new ContainerWithMostWater().maxArea_On(input);
+    end = System.nanoTime();
+    System.out.println("maxArea_On = " + result + ", ET = " + (end-start));
   }
 
 }
