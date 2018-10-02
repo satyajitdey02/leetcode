27bcf24924424342be327ef8835e8075b25659c4
Satyajit Dey <satyajit02@gmail.com>
2018-10-02 23:26:38 +0600

    28. Implement strStr() - Accepted 4ms

--- src/main/java/com/leetcode/problems/StrStr.java
+++ src/main/java/com/leetcode/problems/StrStr.java
@@ -2,4 +2,43 @@ package com.leetcode.problems;
 
 public class StrStr {
 
+  public int strStr(String haystack, String needle) {
+    if (haystack == null || needle == null) {
+      throw new NullPointerException();
+    }
+
+    if (needle.isEmpty() || needle.equals(haystack)) {
+      return 0;
+    }
+
+    if (haystack.isEmpty() || needle.length() > haystack.length()) {
+      return -1;
+    }
+
+    int index = -1;
+    int i = 0;
+    while (i < haystack.length()) {
+      if (haystack.charAt(i) == needle.charAt(0)) {
+        index = i;
+        if (needle.length() > (haystack.length() - index)) {
+          return -1;
+        }
+
+        if (haystack.substring(index, index + needle.length()).equals(needle)) {
+          return index;
+        }
+
+        index = -1;
+      }
+      i++;
+    }
+
+    return index;
+  }
+
+  public static void main(String[] args) {
+    StrStr instance = new StrStr();
+    System.out.println(instance.strStr("mississippi",
+        "sippia"));
+  }
 }
