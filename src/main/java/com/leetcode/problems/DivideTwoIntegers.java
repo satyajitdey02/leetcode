064c0e0c7caa89aa55d48e96e6d181bb19c4c2b0
Satyajit Dey <satyajit02@gmail.com>
2018-10-07 22:18:54 +0600

    29. Divide Two Integers - Accepted 47ms

--- src/main/java/com/leetcode/problems/DivideTwoIntegers.java
+++ src/main/java/com/leetcode/problems/DivideTwoIntegers.java
@@ -26,54 +26,41 @@ public class DivideTwoIntegers {
       return dividend >> 1;
     }
 
-    int c = 0, sign = 0;
+    boolean sign = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
 
-    if (dividend < 0) {
-      dividend = negate(dividend);
-      sign ^= 1;
+    long lDivident = (long) dividend;
+    if (lDivident < 0) {
+      lDivident = -lDivident;
+
     }
 
-    if (divisor < 0) {
-      divisor = negate(divisor);
-      sign ^= 1;
+    long lDivisor = (long) divisor;
+    if (lDivisor < 0) {
+      lDivisor = -lDivisor;
+
     }
 
-    if (divisor != 0) {
-      while (dividend >= divisor) {
-        dividend = sub(dividend, divisor);
-        ++c;
+    int result = 0;
+
+    while (lDivident >= lDivisor) {
+      long temp = lDivisor;
+      long multiply = 1;
+      while (lDivident >= (temp << 1)) {
+        temp = temp << 1;
+        multiply = multiply << 1;
       }
+
+      lDivident = (int) (lDivident - temp);
+      result = (int) (result + multiply);
     }
-    if (sign != 0) {
-      c = negate(c);
-    }
-    return c;
+
+    return sign ? -result : result;
   }
 
-  public int add(int x, int y) {
-
-    while (y != 0) {
-
-      int carry = x & y;
-
-      x = x ^ y;
-
-      y = carry << 1;
-    }
-    return x;
-  }
-
-  public int negate(int x) {
-    return add(~x, 1);
-  }
-
-  public int sub(int x, int y) {
-    return add(x, negate(y));
-  }
 
   public static void main(String[] args) {
-     System.out.println(new DivideTwoIntegers().divide(Integer.MAX_VALUE, 3));
-   // System.out.println(4 << 1);
+    System.out.println(new DivideTwoIntegers().divide(Integer.MAX_VALUE, 3));
+    System.out.println(new DivideTwoIntegers().divide(-2147483648, -3));
   }
 
 }
