bc704923f3fa576ae17ab4c2916a2a1353bcfeaa
Satyajit Dey <satyajit02@gmail.com>
2018-10-09 00:13:12 +0600

    33. Search in Rotated Sorted Array - Another solution but complex

--- src/main/java/com/leetcode/problems/SearchInRotatedSortedArray.java
+++ src/main/java/com/leetcode/problems/SearchInRotatedSortedArray.java
@@ -7,36 +7,25 @@ public class SearchInRotatedSortedArray {
       return -1;
     }
 
-    int start = 0;
-    int end = nums.length - 1;
-
+    int start = 0, end = nums.length - 1;
     while (start <= end) {
       int mid = (start + end) / 2;
-      if (target == nums[mid]) {
+      if (nums[mid] == target) {
         return mid;
       }
-
-      if (nums[start] <= nums[mid]) {
-        if (target >= nums[start] && target < nums[mid]) {
-          end = mid - 1;
-        } else {
-          start = mid + 1;
-        }
+      if ((nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid])) {
+        start = mid + 1;
       } else {
-        if (target > nums[mid] && target <= nums[end]) {
-          start = mid + 1;
-        } else {
-          end = mid - 1;
-        }
+        end = mid - 1;
       }
     }
-
+    
     return -1;
   }
 
   public static void main(String[] args) {
-    int nums[] = {3,1};
-    System.out.println(new SearchInRotatedSortedArray().search(nums, 1));
+    int nums[] = {4, 5, 6, 7, 0, 1, 2};
+    System.out.println(new SearchInRotatedSortedArray().search(nums, 0));
   }
 
 }
