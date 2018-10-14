ff0ddca2cbaf2ef015abdc1180732da8c44999b1
Satyajit Dey <satyajit02@gmail.com>
2018-10-15 00:32:03 +0600

    41. First Missing Positive - 7ms

--- src/main/java/com/leetcode/problems/FirstMissingPositive.java
+++ src/main/java/com/leetcode/problems/FirstMissingPositive.java
@@ -10,6 +10,7 @@ public class FirstMissingPositive {
     if (nums.length < 1) {
       return 1;
     }
+
     Arrays.sort(nums);
 
     if (nums[0] > 1) {
@@ -17,16 +18,12 @@ public class FirstMissingPositive {
     }
 
     for (int i = 1; i < nums.length; i++) {
-      if (nums[i] < 0) {
+      if (nums[i] < 0 || (nums[i - 1] < 0 && nums[i] <= 1)) {
         continue;
       }
 
       if (nums[i - 1] < 0 && nums[i] > 1) {
         return 1;
-      }
-
-      if (nums[i - 1] < 0 && nums[i] <= 1) {
-        continue;
       }
 
       int diff = nums[i] - nums[i - 1];
