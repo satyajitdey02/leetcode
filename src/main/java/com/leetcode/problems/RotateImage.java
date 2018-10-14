fcad9349cc0b3ffe824209e02242814d8d1aa12a
Satyajit Dey <satyajit02@gmail.com>
2018-10-14 19:12:45 +0600

    48. Rotate Image - Accepted 2ms

--- src/main/java/com/leetcode/problems/RotateImage.java
+++ src/main/java/com/leetcode/problems/RotateImage.java
@@ -6,25 +6,27 @@ public class RotateImage {
     int n = matrix.length;
     for (int i = 0; i < n / 2; i++) {
 
-      int nMovements = 4 * (n - 1 - i);
-      int nextI = i;
-      int nextJ = i;
-      int toMove = matrix[nextI][nextJ];
-      for (int j = 0; j < nMovements; j++) {
+      for (int k = 0; k < (n - 1 - 2 * i); k++) {
+        int nMovements = 4 * (n - 1 - 2 * i);
+        int nextI = i;
+        int nextJ = i;
+        int toMove = matrix[nextI][nextJ];
+        for (int j = 0; j < nMovements; j++) {
 
-        if (nextJ + 1 > (n - 1) && nextI < (n - 1)) {
-          nextI++;
-        } else if (nextI + 1 > (n - 1) && nextJ > 0) {
-          nextJ--;
-        } else if (nextJ - 1 < 0 && nextI > 0) {
-          nextI--;
-        } else {
-          nextJ++;
+          if (((nextJ + 1) > (n - 1 - i)) && (nextI < (n - 1 - i))) {
+            nextI++;
+          } else if (((nextI + 1) > (n - 1 - i)) && (nextJ > i)) {
+            nextJ--;
+          } else if (((nextJ - 1) < i) && (nextI > i)) {
+            nextI--;
+          } else {
+            nextJ++;
+          }
+
+          int temp = matrix[nextI][nextJ];
+          matrix[nextI][nextJ] = toMove;
+          toMove = temp;
         }
-
-        int temp = matrix[nextI][nextJ];
-        matrix[nextI][nextJ] = toMove;
-        toMove = temp;
       }
     }
   }
@@ -41,19 +43,34 @@ public class RotateImage {
   }
 
   public static void main(String[] args) {
-    RotateImage instance = new RotateImage();
-  /*  int[][] matrix = {
+    int[][] matrix1 = {
         {1, 2},
         {3, 4}
-    };*/
-    int[][] matrix = {
+    };
+    test1(matrix1);
+
+    int[][] matrix2 = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
     };
-    //instance.printMatrix(matrix);
-    instance.rotate(matrix);
-    instance.printMatrix(matrix);
+    test1(matrix2);
+
+    int[][] matrix3 = {
+        {5, 1, 9, 11},
+        {2, 4, 8, 10},
+        {13, 3, 6, 7},
+        {15, 14, 12, 16}
+    };
+    test1(matrix3);
+  }
+
+  private static void test1(int[][] input) {
+    RotateImage instance = new RotateImage();
+
+    instance.printMatrix(input);
+    instance.rotate(input);
+    instance.printMatrix(input);
   }
 
 }
