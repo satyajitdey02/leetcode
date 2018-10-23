3556ce7e00120dc0cd288bb2493d937603b62a59
satyajitdey02 <satyajit02@gmail.com>
2018-10-23 13:56:36 +0600

    Factorial calculation with different approaches

--- src/main/java/com/leetcode/practice/Factorial.java
+++ src/main/java/com/leetcode/practice/Factorial.java
@@ -1,5 +1,8 @@
 package com.leetcode.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factorial {
 
   private long factorial(int n) {
@@ -10,8 +13,27 @@ public class Factorial {
     return n * factorial(n - 1);
   }
 
+  private int factorialMem(int n) {
+    List<Integer> dp = new ArrayList<>();
+    dp.add(0, 1);
+    for (int i = 1; i <= n; i++) {
+      dp.add(i, dp.get(i - 1) * i);
+    }
+
+    return dp.get(dp.size() - 1);
+  }
+
   public static void main(String[] args) {
     Factorial instance = new Factorial();
-    System.out.println(instance.factorial(10));
+
+    long start = System.nanoTime();
+    long fact1 = instance.factorial(5);
+    long end = System.nanoTime();
+    System.out.println(fact1 + "["+(end-start)+"]");
+
+    start = System.nanoTime();
+    long fact2 = instance.factorialMem(5);
+    end = System.nanoTime();
+    System.out.println(fact2 + "["+(end-start)+"]");
   }
 }
