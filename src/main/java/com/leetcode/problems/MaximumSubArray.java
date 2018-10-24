a77bd43cec7a1b3c4bbcac2c028b67b10fe21ccd
satyajitdey02 <satyajit02@gmail.com>
2018-10-24 18:12:58 +0600

    53. Maximum Subarray - D&C Solution

--- src/main/java/com/leetcode/problems/MaximumSubArray.java
+++ src/main/java/com/leetcode/problems/MaximumSubArray.java
@@ -7,12 +7,14 @@ public class MaximumSubArray {
 
   public static void main(String[] args) {
     MaximumSubArray instance = new MaximumSubArray();
-    instance.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
-    instance.maxSubArray(new int[]{1, 2});
-    instance.maxSubArray(new int[]{-2, 1});
-    instance.maxSubArray(new int[]{8, -19, 5, -4, 20});
+    //System.out.println(instance.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    System.out.println(instance.maxSubArray(new int[]{1, 2}));
+    //System.out.println(instance.maxSubArray(new int[]{-2, 1}));
+    //System.out.println(instance.maxSubArray(new int[]{8, -19, 5, -4, 20}));
   }
 
+  /*
+  //O(n) solution
   public int maxSubArray(int[] nums) {
     int sum = nums[0];
     int sumMax = sum;
@@ -27,6 +29,42 @@ public class MaximumSubArray {
     }
 
     return sumMax;
+  }*/
+
+  //Divide & Conquer solution
+  private int divide(int[] nums, int start, int end) {
+    int maxSum = nums[start];
+    if (end > start) {
+      int mid = start + (end - start) / 2;
+      divide(nums, start, mid);
+      divide(nums, mid + 1, end);
+    }
+
+    maxSum = conquer(nums, start, end, maxSum);
+    return maxSum;
+  }
+
+  private int conquer(int[] nums, int start, int end, int maxSum) {
+    int sumMax = maxSum;
+    if (start == end) {
+      return Math.max(sumMax, nums[start]);
+    }
+
+    int sum = nums[start];
+    for (int i = start + 1; i <= end; i++) {
+      sum += nums[i];
+      if (nums[i] > sum) {
+        sum = nums[i];
+      }
+
+      sumMax = Math.max(sumMax, sum);
+    }
+
+    return sumMax;
+  }
+
+  public int maxSubArray(int[] nums) {
+    return divide(nums, 0, nums.length - 1);
   }
 
 }
