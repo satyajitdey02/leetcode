38a79662ebf6720b6d302cbb931ab98b18be1302
satyajitdey02 <satyajit02@gmail.com>
2018-11-28 16:13:23 +0600

    66. Plus One - BEST

--- src/main/java/com/leetcode/problems/PlusOne.java
+++ src/main/java/com/leetcode/problems/PlusOne.java
@@ -7,31 +7,24 @@ public class PlusOne {
 
   public static void main(String[] args) {
     PlusOne instance = new PlusOne();
-    System.out.println(instance.plusOne(new int[]{8, 9, 9, 8}));
+    System.out.println(instance.plusOne(new int[]{9}));
   }
 
   public int[] plusOne(int[] digits) {
 
-    if (digits[digits.length - 1] < 9) {
-      digits[digits.length - 1]++;
-      return digits;
+    int n = digits.length;
+    for (int i = n - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+
+      digits[i] = 0;
     }
 
-    int carry = 1;
-    for (int i = digits.length - 1; i >= 0; i--) {
-      int sum = digits[i] + carry;
-      digits[i] = sum % 10;
-      carry = sum / 10;
-    }
+    int[] newNumber = new int[n + 1];
+    newNumber[0] = 1;
 
-    if (carry == 0) {
-      return digits;
-    }
-
-    int[] result = new int[digits.length + 1];
-    System.arraycopy(digits, 0, result, 1, digits.length);
-    result[0] = carry;
-
-    return result;
+    return newNumber;
   }
 }
